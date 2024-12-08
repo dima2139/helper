6c38070e811d013f0afe839c6eb274321ae1cc96
Dmytro Shchurovskyi <nutzername.dmitriy@gmail.com>
2024-12-08 01:41:20 +0100

    added login page

--- helper/src/pages/Login.tsx
+++ helper/src/pages/Login.tsx
@@ -1,12 +1,35 @@
-import { Input } from "antd";
+import { Button, Input } from "antd";
+import axios from "axios";
+import { useState } from "react";
+
 
 function Login() {
+
+    const handleLoginClick = () => {
+        axios
+        .post('http://localhost:3001/login', {Login : "dima"})
+        .then((response) => {
+            setInputValue("")
+        })
+        .catch((err) => {
+          
+        });
+      };
+    
+    const [inputValue, setInputValue] = useState("");
+    
+        // Step 2: Handle input changes
+        const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputValue(event.target.value); // Update state with the current input value
+        };
+    
     return (
         <>  
             <b>Login</b>
-            <Input size="large" />
+            <Input size="large" type="text" value={inputValue} onChange={handleInputChange} />
             <b>Password</b>
             <Input size="large" />
+            <Button onClick={handleLoginClick}>Login</Button>
         </>
     );
   }
